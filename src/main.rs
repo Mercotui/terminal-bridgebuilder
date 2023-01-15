95e75badd3ba092bf8b16a1d2657254aae5df6ab
Menno van der Graaf <mennovandergraaf@hotmail.com>
2023-01-16 00:27:31 +0100

    Switch logging library and add instructions on logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use crate::stop_token::StopToken;
 use crate::ui::Gui;
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::Parser;
 use signal_hook::{consts::SIGINT, iterator::Signals};
+use std::io;
+use std::ops::Deref;
+use std::panic;
 use std::sync::Arc;
 use std::thread;
+use tracing::error;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
 mod engine;
 mod level;
@@ -18,9 +23,6 @@ mod ui;
 struct Cli {
     /// The path to a level to load
     level_path: std::path::PathBuf,
-    /// The level of logging verbosity
-    #[clap(flatten)]
-    verbose: clap_verbosity_flag::Verbosity,
 }
 
 fn install_signal_handler(stop_token: Arc<StopToken>) -> Result<()> {
@@ -34,9 +36,45 @@ fn install_signal_handler(stop_token: Arc<StopToken>) -> Result<()> {
     Ok(())
 }
 
+fn configure_logger() -> Result<()> {
+    // Create log printer that writes to STDERR
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(io::stderr)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .context("setting default subscriber failed")?;
+
+    // Configure logging of panics
+    panic::set_hook(Box::new(|panic_info| {
+        let (filename, line) = panic_info
+            .location()
+            .map(|loc| (loc.file(), loc.line()))
+            .unwrap_or(("<unknown>", 0));
+
+        let cause = panic_info
+            .payload()
+            .downcast_ref::<String>()
+            .map(String::deref);
+
+        let cause = cause.unwrap_or_else(|| {
+            panic_info
+                .payload()
+                .downcast_ref::<&str>()
+                .copied()
+                .unwrap_or("<cause unknown>")
+        });
+
+        error!("A panic occurred at {}:{}: {}", filename, line, cause);
+    }));
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
-    env_logger::init();
     let args = Cli::parse();
+    configure_logger()?;
 
     let stop_token = Arc::new(StopToken::new());
     install_signal_handler(stop_token.clone())?;
